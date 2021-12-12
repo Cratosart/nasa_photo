d19d6e8d85fa5b1fa54001b0832c3dd0ec0475c9
Artyom <artem.a.dyachenko@gmail.com>
2021-12-12 20:48:41 +0300

    New features
    1. Added argparse function
    2 .Update README.md

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
+import sys
+
 import requests
 import os
 import datetime
 import urllib.request
 import uuid
 import urllib
-from dotenv import load_dotenv
 import telegram
+import time
+import argparse
+
+
+from os.path import isfile
+from os.path import join as joinpath
+from dotenv import load_dotenv
+from os import listdir
 
 load_dotenv()
 
-api_key = os.environ['API_KEY_NASA']
+api_key_nasa = os.environ['API_KEY_NASA']
+api_key_telegram = os.environ['API_KEY_TELEGRAM']
 
+
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('time', nargs='?', default=86400)
+    return parser
 
 def nasa_epic_requests(url):
     payload = {}
@@ -38,25 +53,31 @@ def image_save(url, path_save):
     filename = str(uuid.uuid4())
     urllib.request.urlretrieve(url, f'./{path_save}/{filename}.png')
 
+if __name__ == '__main__':
+    images_path_nasa = 'images_nasa'
+    if not os.path.exists(images_path_nasa):
+        os.mkdir(images_path_nasa)
 
-images_path_nasa = 'images_nasa'
-if not os.path.exists(images_path_nasa):
-    os.mkdir(images_path_nasa)
+    url_epic_nasa = 'https://api.nasa.gov/EPIC/api/natural?api_key=cz0rECf7cwW5n90a51pe01jDNPuPAlgM9Wtp0KWa'
 
-url_epic_nasa = 'https://api.nasa.gov/EPIC/api/natural?api_key=cz0rECf7cwW5n90a51pe01jDNPuPAlgM9Wtp0KWa'
+    nasa_epic = nasa_epic_requests(url_epic_nasa).json()
+    for name in nasa_epic:
+        image_date = nasa_epic_date(name)
+        image_name = nasa_epic_image(name)
+        url_epic_image_nasa = f'https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{image_name}.png?api_key={api_key_nasa}'
+        image_save(url_epic_image_nasa, images_path_nasa)
 
-nasa_epic = nasa_epic_requests(url_epic_nasa).json()
-for name in nasa_epic:
-    image_date = nasa_epic_date(name)
-    image_name = nasa_epic_image(name)
-    url_epic_image_nasa = f'https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{image_name}.png?api_key={api_key}'
-    image_save(url_epic_image_nasa, images_path_nasa)
 
-bot = telegram.Bot(token='5053046789:AAFYgBv0puCAOBY9hTPtYordwbHrhPduZEM')
-print(bot.get_me())
-updates = bot.get_updates()
-print(updates[0])
-bot.send_message(chat_id='@NASA_PHOTO',
-                 text="I'm sorry Dave I'm afraid I can't do that.")
-bot.send_document(chat_id='@NASA_PHOTO',
-                  document=open(f'./images_nasa/0c6aa032-7367-4377-8132-9cc9c1806b82.png', 'rb'))
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+    sleep_time = namespace.time
+
+    mypath = "./images_nasa"
+    for image in listdir(mypath):
+        if isfile(joinpath(mypath,image)):
+
+
+            bot = telegram.Bot(token=f'{api_key_telegram}')
+            time.sleep(sleep_time)
+            bot.send_document(chat_id='@NASA_PHOTO',
+                              document=open(f'./images_nasa/{image}', 'rb'))
