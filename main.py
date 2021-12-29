89eb7f094b2c0694ab00e08542afce61b0c1b724
Artyom <artem.a.dyachenko@gmail.com>
2021-12-29 20:58:29 +0300

    format pep8

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-import sys
-import requests
-import os
+import argparse
 import datetime
-import urllib.request
-import uuid
-import urllib
+import os
+import requests
+import sys
 import telegram
 import time
-import argparse
+import urllib
 import urllib.parse
+import urllib.request
+import uuid
 
-from urllib.parse import urlparse
-from os.path import isfile
-from os.path import splitext
-from os.path import join as joinpath
 from dotenv import load_dotenv
 from os import listdir
+from os.path import isfile
+from os.path import join as joinpath
+from os.path import splitext
+from urllib.parse import urlparse
 
 
 def createparser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('time', nargs='?', default=10)
+    parser.add_argument('time', nargs='?', default=84600)
     return parser
 
 
@@ -29,7 +29,8 @@ def save_image(url, path_save):
     urllib.request.urlretrieve(url, f'./{path_save}/{filename}.png')
 
 
-def fetch_spacex_last_launch(url):
+def fetch_spacex_last_launch():
+    url = 'https://api.spacexdata.com/v3/launches/65'
     response = requests.get(url)
     response.raise_for_status()
     info = response.json()['links']
@@ -48,33 +49,30 @@ def get_image_url_nasa(image_name, image_date):
     return response.url
 
 
-def save_nasa_image(nasa_epic, image_name,image_date):
-        format_url_nasa(nasa_epic)
-        url_epic_image_nasa = get_image_url_nasa(image_name, image_date)
-        save_image(url_epic_image_nasa, images_path_nasa)
+def save_nasa_image(image_name, image_date):
+    url_epic_image_nasa = get_image_url_nasa(image_name, image_date)
+    save_image(url_epic_image_nasa, images_path_nasa)
 
-def format_url_nasa(nasa_epic):
+
+def fetch_nasa_epic_images(api_key):
+    url = 'https://api.nasa.gov/EPIC/api/natural'
+    payload = {'api_key': api_key}
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    nasa_epic = response.json()
     for name in nasa_epic:
-        image_date = format_nasa_epic_date(name)
-        image_name = get_nasa_epic_image(name)
-        return image_name, image_date
+        for key, value in name.items():
+            if key == "date":
+                value = datetime.datetime.fromisoformat(value)
+                image_date = value.strftime('%Y/%m/%d')
+        for key, value in name.items():
+            if key == "image":
+                image_name = value
+        save_nasa_image(image_name, image_date)
 
-def fetch_nasa_epic_images(url, api_key):
-        payload = {'api_key': api_key}
-        response = requests.get(url, params=payload)
-        response.raise_for_status()
-        nasa_epic = response.json()
-        for name in nasa_epic:
-            for key, value in name.items():
-                if key == "date":
-                    value = datetime.datetime.fromisoformat(value)
-                    image_date = value.strftime('%Y/%m/%d')
-            for key, value in name.items():
-                if key == "image":
-                    image_name = value
-            save_nasa_image(nasa_epic, image_name, image_date)
 
-def fetch_nasa_apod_images(url_apod_nasa, api_key_nasa, path_save):
+def fetch_nasa_apod_images(api_key_nasa, path_save):
+    url_apod_nasa = 'https://api.nasa.gov/planetary/apod'
     payload = {'api_key': api_key_nasa,
                'count': 30}
     response = requests.get(
@@ -93,27 +91,18 @@ def fetch_nasa_apod_images(url_apod_nasa, api_key_nasa, path_save):
 
 
 if __name__ == '__main__':
-
     load_dotenv()
     api_key_nasa = os.environ['API_KEY_NASA']
     api_key_telegram = os.environ['API_KEY_TELEGRAM']
     chat_id = os.environ['CHAT_ID_TELEGRAM']
-
     images_path_nasa = 'images_nasa'
     os.makedirs(images_path_nasa, exist_ok=True)
-
-    url_epic_nasa = 'https://api.nasa.gov/EPIC/api/natural'
-    url_apod_nasa = 'https://api.nasa.gov/planetary/apod'
-    fetch_nasa_apod_images(url_apod_nasa, api_key_nasa, images_path_nasa)
-    fetch_nasa_epic_images(url_epic_nasa, api_key_nasa)
-
+    fetch_nasa_apod_images(api_key_nasa, images_path_nasa)
+    fetch_nasa_epic_images(api_key_nasa)
     parser = createparser()
     namespace = parser.parse_args()
     sleep_time = namespace.time
-
-    url_spacex = 'https://api.spacexdata.com/v3/launches/65'
-    fetch_spacex_last_launch(url_spacex)
-
+    fetch_spacex_last_launch()
     path = "./images_nasa"
     for image in listdir(path):
         if isfile(joinpath(path, image)):
